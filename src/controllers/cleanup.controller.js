33d965bed24d66330e7a00589206dfbec8bf6754
thomasbarenghi <thomasbarenghi@gmail.com>
2026-06-23 22:19:10 -0300

    Add cleanup and seed services for database management and dummy data generation

--- src/controllers/cleanup.controller.js
+++ src/controllers/cleanup.controller.js
@@ -1,11 +1,8 @@
-import { User, Post, Comment, Tag } from '../models/index.js'
+import * as cleanupService from '../services/cleanup.service.js'
 
 export const dropAllCollections = async (req, res, next) => {
   try {
-    await User.deleteMany({})
-    await Post.deleteMany({})
-    await Comment.deleteMany({})
-    await Tag.deleteMany({})
+    await cleanupService.dropAllCollections()
     res.json({ message: 'Todas las colecciones han sido eliminadas' })
   } catch (err) {
     next(err)
